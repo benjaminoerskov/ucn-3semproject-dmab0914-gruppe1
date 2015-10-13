8f79014aa0955f47d8315d8fa1b893724167c56e
Kantraz <lassesl93@gmail.com>
2015-10-13 12:07:48 +0200

    Model Layer Objects

--- ShoppingList/ShoppingList/Form1.Designer.cs
+++ ShoppingList/ShoppingList/Form1.Designer.cs
@@ -28,13 +28,27 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.collapsiblePanel1 = new OVT.CustomControls.CollapsiblePanel();
             this.SuspendLayout();
+            // 
+            // collapsiblePanel1
+            // 
+            this.collapsiblePanel1.BackColor = System.Drawing.Color.Transparent;
+            this.collapsiblePanel1.HeaderFont = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold);
+            this.collapsiblePanel1.HeaderImage = null;
+            this.collapsiblePanel1.HeaderText = "CollapsiblePanel";
+            this.collapsiblePanel1.HeaderTextColor = System.Drawing.Color.Black;
+            this.collapsiblePanel1.Location = new System.Drawing.Point(12, 12);
+            this.collapsiblePanel1.Name = "collapsiblePanel1";
+            this.collapsiblePanel1.Size = new System.Drawing.Size(250, 150);
+            this.collapsiblePanel1.TabIndex = 0;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(821, 492);
+            this.Controls.Add(this.collapsiblePanel1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -42,6 +56,8 @@
         }
 
         #endregion
+
+        private OVT.CustomControls.CollapsiblePanel collapsiblePanel1;
     }
 }
 
